12b4a8eb90e76a4e6c33f894388f6db513738033
Developer-Rajesh100 <rajeshhabra100@gmail.com>
2023-06-14 18:06:01 +0530

    Route, Switch, Link remove from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Shared/Navbar/Navbar";
 import AllRoutes from "./AllRoutes";
 
